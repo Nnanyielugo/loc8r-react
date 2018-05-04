bee89532d4c8315317f38a873d6ec6511f7bd0ae
Nnanyielugo <kz.onyegbule@gmail.com>
2018-05-04 18:07:18 +0100

    create location

--- api/controllers/locations.js
+++ api/controllers/locations.js
@@ -20,37 +20,73 @@ const earth = (function(){
 module.exports.listLocationsByDistance = (req, res, next) => {
   let longitude = parseFloat(req.query.longitude);
   let latitude = parseFloat(req.query.latitude);
-  let maxDistance = parseFloat(req.query.maxDistance);
+  // let maxDistance = parseFloat(req.query.maxDistance);
 
   let point = {
     type: "Point",
     coordinates: [longitude, latitude]
   }
 
-  let geoOptions = {
-    spherical: true,
-    maxDistance: earth.getRadiansFromDistance(maxDistance),
-    num: 10
-  }
 
-  if((!longititude && longitude === 0) || (!latitude && latitude === 0)){
+  if((!longitude && longitude === 0) || (!latitude && latitude === 0)){
     console.log('LocationListByDistance missing params');
     return res.status(401).json({message: "longitude and latitude query parameters are required"})
   }
 
-  Locations.geoNear(point, geoOptions, (err, results, stats)=> {
-    let locations = [];
-    if(err) return next(err)
-    results.forEach(result => {
-      locations.push({
-        distance: result.dis,
-        name: result.obj.name,
-        address: result.obj.address,
-        rating: result.obj.rating,
-        facilities: result.obj.facilities,
-        id: result.obj._id
+  Locations.aggregate([
+      {
+        '$geoNear': {
+          'near': point,
+          'spherical': true,
+          'distanceField': 'dist',
+          'num': 10,
+          'maxDistance': earth.getRadiansFromDistance(20)
+        }
+      }
+    ], (err, results, stats)=> {
+      let locations = [];
+      if(err) return next(err)
+      results.forEach(result => {
+        console.log(result)
+        locations.push({
+          distance: result.dis,
+          name: result.name,
+          address: result.address,
+          rating: result.rating,
+          facilities: result.facilities,
+          id: result._id
+        })
       })
+      res.status(200).send(locations)
+    });
+}
+
+module.exports.createLocation = (req, res, next) => {
+  const location = new Locations({
+    name: req.body.name,
+    address: req.body.address,
+    facilities: req.body.facilities.split(","),
+    coords: [parseFloat(req.body.longitude), parseFloat(req.body.latitude)],
+    openingTimes: [{
+      days:req.body.days1,
+      opening: req.body.opening1,
+      closing: req.body.closing1,
+      closed: req.body.closed1,
+    },{
+      days: req.body.days2,
+      opening: req.body.opening2,
+      closing: req.body.closing2,
+      closed: req.body.closed2,
+    }]
+  });
+
+  // 3.601521 6.458985
+
+  return location.save()
+    .then(() => {
+      console.log(location)
+      return res.json({ location: location})
     })
-    res.status(200).send(locations)
-  })
+    .catch(next);
+
 }
--- api/routes/locations.js
+++ api/routes/locations.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const auth = require('../helpers/auth');
 const locations = require('../controllers/locations');
 
-router.get('/', locations.listLocationsByDistance)
+router.get('/', locations.listLocationsByDistance);
+router.post('/', locations.createLocation);
 
 module.exports = router;
